31cb03a253c3837448efeab79bf0053d5664b157
xiong123coder <1940664864@qq.com>
2025-08-06 21:58:08 +0800

    保存本地修改，准备合并远程

--- C_Real_Exam_Coding/gcd_and_lcm/gcd_lcm_function.c
+++ C_Real_Exam_Coding/gcd_and_lcm/gcd_lcm_function.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 int gcd(int a,int b)
 {   
-    if(b%a==0)
+    if(b==0)
     {
         printf("a:%d就是最大公约数\n",a);
         return a;
     }
     else
     {   
-        printf("此时的a是%d,b是%d\n",b,b%a);
-        return gcd(a,b%a);
+        printf("调用gcd(%d, %d)\n",b%a,a);
+        return gcd(b%a,a);
     }
 }
 int main()
@@ -20,7 +20,9 @@ int main()
     return 0;
 }
 /*关键错误反思；
-原本错误代码：return gcd(b, b % a);  // 错误：下一次递归把较大的数又放前面了
+!原本错误代码：return gcd(b, b % a);  // 错误：下一次递归把较大的数又放前面了
 我以为保持b在前面就行，实际上要把当前的余数（b % a）放在前面，原本较小的a放在后面
 ，才能实现“不断缩小问题规模”的目标
+!我原代码：if (b % a == 0)终止条件写法有隐患
+！建议以后调试时打印这些：printf("调用gcd(%d, %d)\n", a, b);
 */
